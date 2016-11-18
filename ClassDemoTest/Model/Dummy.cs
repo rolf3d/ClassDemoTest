0d23ff2e9ff315d5e24af94de8a3942a1ec78e93
Rolf Petersen <rollep7@msn.com>
2016-11-18 11:08:32 +0100

    Unit test

---
+++ ClassDemoTest/Model/Dummy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoTest.Model
+{
+    // Dummy class
+    public class Dummy
+    {
+        private string sometext;
+        
+
+        public string Sometext
+        {
+            get { return sometext; }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("sometext must be null ore empty");
+                }
+                sometext = value;
+            }
+        }
+
+        private int somenumber;
+
+        public int SomeNumber
+        {
+            get { return somenumber; }
+            set
+            {
+                if (value < 1 || 4 < value)
+                {
+                    throw new ArgumentException("Somenumber must be in range 1-4");
+                }
+                somenumber = value;
+            }
+        }
+    }
+}
---
+++ TestDummy/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using ClassDemoTest.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestDummy
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //test methods
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Dummy dum = new Dummy();
+            
+            dum.SomeNumber = 1;
+            Assert.AreEqual(1, dum.SomeNumber);
+            
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Dummy dum = new Dummy();
+            try
+            {
+                dum.SomeNumber = 0;
+                Assert.Fail();
+            }
+            catch (ArgumentException ae)
+            {
+                
+                // ok
+            }
+
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Dummy dum = new Dummy();
+            
+                dum.SomeNumber = 4;
+                Assert.AreEqual(4, dum.SomeNumber);
+           
+
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Dummy dum = new Dummy();
+            try
+            {
+                dum.SomeNumber = 5;
+                Assert.Fail();
+            }
+            catch (ArgumentException ae)
+            {
+
+                // ok
+            }
+
+
+        }
+    }
+}
